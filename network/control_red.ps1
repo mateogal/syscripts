90ab5593dcca8db25982451405682f631ba5e726
Mateo Galagorri <soporte@newquality.com.uy>
2024-09-11 17:32:29 -0300

    Update control_red.ps1

--- network/control_red.ps1
+++ network/control_red.ps1
@@ -1,83 +1,83 @@
-#Requisito previo, ejecutar en PS en todos los equipos a conectar: Enable-PSRemoting
+#Pre-requisites in all machines: Enable-PSRemoting
 
-$host_remoto_name = "Srv-Host" #Nombre del equipo DNS
-$host_remoto = "192.168.1.14" #Interfaz Principal a chequear
-$host_remoto_bkp = "192.168.1.18" #Interfaz secundaria a usar para conexion en caso de fallo
-$interfaz_name = "Ethernet" #Nombre del adaptador a reinciar
-$host_remoto_username = "Srv-Host\Administrador" #Credenciales del equipo a conectarse
-$host_remoto_password = ConvertTo-SecureString "PASSWORD" -AsPlainText -Force #Credenciales del equipo a conectarse
-$credenciales = New-Object System.Management.Automation.PSCredential($host_remoto_username, $host_remoto_password)
+$remote_hostname = "Srv-Host" # Hostname
+$remote_host = "192.168.1.14" # Main network interface to check
+$remote_host_bkp = "192.168.1.18" # Secondary network interface to check in case of failure
+$interfaz_name = "Ethernet" # Adapter name to restart
+$remote_host_username = "Srv-Host\Administrador" # Remote machine username
+$remote_host_password = ConvertTo-SecureString "PASSWORD" -AsPlainText -Force # Remote machine password
+$credentials = New-Object System.Management.Automation.PSCredential($remote_host_username, $remote_host_password)
 
-if ($host_remoto -NotLike "*$($host_remoto)*") {
-    Set-Item WSMan:\localhost\Client\TrustedHosts -Concatenate -Value $host_remoto -Force #Agregamos a TrustedHosts el equipo a conectarse principal
+if ($remote_host -NotLike "*$($remote_host)*") {
+    Set-Item WSMan:\localhost\Client\TrustedHosts -Concatenate -Value $remote_host -Force # Add TrustedHosts
 }
 
-if ($host_remoto -NotLike "*$($host_remoto_bkp)*") {
-    Set-Item WSMan:\localhost\Client\TrustedHosts -Concatenate -Value $host_remoto_bkp -Force #Agregamos a TrustedHosts el equipo a conectarse secundaria
+if ($remote_host -NotLike "*$($remote_host_bkp)*") {
+    Set-Item WSMan:\localhost\Client\TrustedHosts -Concatenate -Value $remote_host_bkp -Force # Add TrustedHosts
 }
 
-$test_connection_host = Test-NetConnection -ComputerName $host_remoto -InformationLevel Detailed #PING principal
-$test_connection_host_bkp = Test-NetConnection -ComputerName $host_remoto_bkp -InformationLevel Detailed #PING secundaria
+$test_connection_host = Test-NetConnection -ComputerName $remote_host -InformationLevel Detailed # PING to main
+$test_connection_host_bkp = Test-NetConnection -ComputerName $remote_host_bkp -InformationLevel Detailed # PING to secondary
 
-Write-Output "Comenzado chequeo de $($host_remoto) / $($host_remoto_bkp) / Interfaz: $($interfaz_name) `n" > "C:\scripts\check_red.log"
-Write-Output "PING interfaz principal $($host_remoto):" >> "C:\scripts\check_red.log"
-Write-Output $($test_connection_host | Format-List) >> "C:\scripts\check_red.log"
+Write-Output "Starting test: $($remote_host) / $($remote_host_bkp) / Interfaz: $($interfaz_name) `n" > "C:\scripts\check_network.log"
+Write-Output "PING main interface: $($remote_host):" >> "C:\scripts\check_network.log"
+Write-Output $($test_connection_host | Format-List) >> "C:\scripts\check_network.log"
 
 if (!$test_connection_host.PingSucceeded) {
-    Write-Output "El Host destino NO respondio en interfaz principal $($host_remoto) `n" >> "C:\scripts\check_red.log"
-    Write-Output "PING interfaz secundaria $($host_remoto_bkp):" >> "C:\scripts\check_red.log"
-    Write-Output $($test_connection_host_bkp | Format-List) >> "C:\scripts\check_red.log"
+    Write-Output "Destination host ping fail: $($remote_host) `n" >> "C:\scripts\check_network.log"
+    Write-Output "PING secondary interface: $($remote_host_bkp):" >> "C:\scripts\check_network.log"
+    Write-Output $($test_connection_host_bkp | Format-List) >> "C:\scripts\check_network.log"
 
     if ($test_connection_host_bkp.PingSucceeded) {
-        $session = New-PSSession $host_remoto_bkp -Credential $credenciales
+        $session = New-PSSession $remote_host_bkp -Credential $credentials
 
-        Write-Output "Conectado al host remoto por interfaz secundaria $($host_remoto_bkp). Comenzando con los comandos. `n" >> "C:\scripts\check_red.log"
+        Write-Output "Connected to remote host through secondary interface: $($remote_host_bkp). Fix starting. `n" >> "C:\scripts\check_network.log"
 
         $adapter = Invoke-Command -Session $session -ScriptBlock { Get-NetAdapter -Name $Using:interfaz_name }
 
         $disable_adapter = Invoke-Command -Session $session -ScriptBlock { Disable-NetAdapter -Name $Using:adapter.Name -Confirm:$false }
-        Write-Output "Comando Disable-NetAdapter: $($disable_adapter). `n" >> "C:\scripts\check_red.log"
+        Write-Output "Command Disable-NetAdapter: $($disable_adapter). `n" >> "C:\scripts\check_network.log"
 
         $enable_adapter = Invoke-Command -Session $session -ScriptBlock { Enable-NetAdapter -Name $Using:adapter.Name -Confirm:$false }
-        Write-Output "Comando Enable-NetAdapter: $($enable_adapter). `n" >> "C:\scripts\check_red.log"
+        Write-Output "Command Enable-NetAdapter: $($enable_adapter). `n" >> "C:\scripts\check_network.log"
 
         Remove-PSSession $session
 
         Start-Sleep -Seconds 5
 
-        $test_connection_host = Test-NetConnection -ComputerName $host_remoto -InformationLevel Detailed
-        Write-Output "PING luego de intento solucionar:" >> "C:\scripts\check_red.log"
-        Write-Output $($test_connection_host | Format-List) >> "C:\scripts\check_red.log"
+        $test_connection_host = Test-NetConnection -ComputerName $remote_host -InformationLevel Detailed
+        Write-Output "PING after fix attempt:" >> "C:\scripts\check_network.log"
+        Write-Output $($test_connection_host | Format-List) >> "C:\scripts\check_network.log"
 
         if ($test_connection_host.PingSucceeded) {
-            $resultado = "SOLUCIONADO"
-            Write-Output "Fue solucionado el problema." >> "C:\scripts\check_red.log"
+            $result = "FIXED"
+            Write-Output "Problem fixed." >> "C:\scripts\check_network.log"
         }
         else {
-            $resultado = "ADVERTENCIA CONECTADO PERO NO SOLUCIONADO"
-            Write-Output "NO se pudo solucionar el problema." >> "C:\scripts\check_red.log"
+            $result = "WARNING: CONNECTED BUT NOT FIXED"
+            Write-Output "Problem wasn't fixed." >> "C:\scripts\check_network.log"
         }        
     }
     else {
-        Write-Output "El Host destino NO respondio en interfaz secundaria $($host_remoto_bkp) `n" >> "C:\scripts\check_red.log"
-        Write-Output "NINGUNA interfaz del host remoto respondio." >> "C:\scripts\check_red.log"
+        Write-Output "Remote host didn't respond on secondary interface $($remote_host_bkp) `n" >> "C:\scripts\check_network.log"
+        Write-Output "None of the interfaces from remote host responded" >> "C:\scripts\check_network.log"
 
-        $resultado = "EMERGENCIA TODAS INTERFACES CAIDAS"
+        $result = "EMERGENCY ALL REMOTE INTERFACES ARE DOWN"
     }
 
-    #ENVIANDO MAIL CON ADJUNTO
+    # Send email
     $fecha = Get-Date
     $from = "EMAIL"
     $to = "EMAIL"
-    $subject = "$($resultado) / Chequeo de Red - XXXXXX / $($host_remoto_name)"
+    $subject = "$($result) / Network check - XXXXXX / $($remote_hostname)"
     $smtpserver = "SMTP"
     $user = "EMAIL"
     $passwd = ConvertTo-SecureString "PASSWORD" -AsPlainText -Force
-    $credenciales = New-Object System.Management.Automation.PSCredential ($user, $passwd)
+    $credentials = New-Object System.Management.Automation.PSCredential ($user, $passwd)
     $port = 587
-    Send-MailMessage -smtpServer $smtpserver -from $from -to $to -subject $subject -body "$fecha" -credential $credenciales -Attachments "C:\scripts\check_red.log" -UseSsl -Port $port
+    Send-MailMessage -smtpServer $smtpserver -from $from -to $to -subject $subject -body "$fecha" -credential $credentials -Attachments "C:\scripts\check_network.log" -UseSsl -Port $port
 
 }
 else {
-    Write-Output "El Host esta activo sin problemas." >> "C:\scripts\check_red.log"
+    Write-Output "Remote host is running without problems." >> "C:\scripts\check_network.log"
 }
