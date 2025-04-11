5e511e15028b33764e5989052c099b4e0b8d70bc
Mateo Galagorri <mateo.galagorri@gmail.com>
2025-04-11 17:57:47 -0300

    virsh-backups

--- backups/virsh-backups.sh
+++ backups/virsh-backups.sh
@@ -4,26 +4,32 @@
 echo "$(date +"%Y-%m-%d %H:%M:%S"): Backups started" > /tmp/virsh-backups.log 2>&1
 
 VM_ARRAY=("VM1" "VM2" "VM3")
-RUNNING=0
 BKP_PATH="/VMs-BKP" # Directory to move backups when it's finished
+VM_PENDING=("${VM_ARRAY[@]}")
 
 for vm in "${VM_ARRAY[@]}"; do
     echo "$(date +"%Y-%m-%d %H:%M:%S"): Backup for $vm started" >> /tmp/virsh-backups.log 2>&1
     virsh backup-begin "$vm"
-    ((RUNNING++))
 done
 
-while [[ $RUNNING -gt 0 ]]; do
-    for vm in "${VM_ARRAY[@]}"; do
-        # Change "None" to the correct word for your OS language
-        check=$(virsh domjobinfo "$vm" | grep "None")
-        if [[ -n $check ]]; then
-            echo "$(date +"%Y-%m-%d %H:%M:%S"): Backup for $vm finished" >> /tmp/virsh-backups.log 2>&1
-            ((RUNNING--))
-        fi
-    done
-    sleep 5
+i=0
+while [[ ${#VM_PENDING[@]} -gt 0 ]]; do
+    vm="${VM_PENDING[$i]}"
+    # Change "None" to the correct word for your OS language
+    check=$(virsh domjobinfo "$vm" 2>/dev/null | grep "None")
+
+    if [[ -n $check ]]; then
+        echo "$(date +"%Y-%m-%d %H:%M:%S"): Backup for $vm finished" >> /tmp/virsh-backups.log 2>&1
+        unset 'VM_PENDING[i]'
+        VM_PENDING=("${VM_PENDING[@]}")
+        i=0
+    else
+        ((i++))
+        [[ $i -ge ${#VM_PENDING[@]} ]] && i=0
+    fi
+    sleep 1
 done
+
 
 for vm in "${VM_ARRAY[@]}"; do
     mkdir -p "$BKP_PATH/$vm"
